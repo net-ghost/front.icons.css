08cc32d46acdd4d9f58b39d9089cda1d6c8e575c
netghost <60497242+net-ghost@users.noreply.github.com>
2020-07-20 12:15:50 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html><html lang=es>
+	<head>
+	
+	<link rel="stylesheet" type="text/css" href="wp-includes/css/tp-cont.css">
+	<link rel=preload as=script><meta charset=UTF-8>
+	<link rel='shortcut icon' href="img/icon.png" type=image/x-icon>
+	<title>Pelis E'd</title>
+	<link rel=stylesheet id=owl-carousel-css href="wp-content/themes/css/front.owl.css" type=text/css media=all>
+	<link rel=stylesheet id=fonts-css href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700' type=text/css media=all>
+	<link rel=stylesheet id=icons-css href="https://repelishd.tv/wp-content/themes/dooplay/assets/css/front.icons.css" type=text/css media=all>
+	<link rel=stylesheet id=scrollbar-css href="wp-content/themes/css/front.crollbar.css" type=text/css media=all>
+	<link rel=stylesheet id=theme-css href="wp-content/themes/css/front.style.css" type=text/css media=all><link rel=stylesheet id=color-scheme-css href="wp-content/themes/css/colors.dark.css" type=text/css media=all>
+	<link rel=stylesheet id=responsive-css href="wp-content/themes/css/colors.dark.css" type=text/css media=all>
+	 <script src="wp-includes/js/jquery/jquery.js"></script> <script src="wp-includes/js/jquery/jquery-migrate.min.js"></script> <script src="wp-content/themes/js/lib/mcsbscrollbar.js"></script> <script src="wp-content/themes/js/lib/owlcarousel.js"></script> 
+	</head>
+
+
+<style>.featu{background:#000} body{background: url(https://i.ibb.co/s1qY5NX/Background.png)}</style>
+
+
+
+
+
+<body class="home blog" >
+	<div id=dt_contenedor>
+		<?php
+		include ("wp-includes/p-p/cabezaweb.php");
+		include ("wp-includes/p-p/cabezamob.php");
+		?>
+				
+
+<!-- 1 FILA -->
+
+<?php
+include ("wp-includes/p-p/fila1.php");
+?>			
+
+<!-- 1 FILA -->
+
+<!-- 2 FILA -->
+
+<?php
+include ("wp-includes/p-p/fila2.php");
+?>	
+
+<!-- 2 FILA -->
+
+<!-- 3 FILA -->
+
+<?php
+include ("wp-includes/p-p/fila3.php");
+?>					
+
+
+<!-- 3 FILA -->
+
+
+<script>jQuery(document).ready(function($){var owl=$("#genre_actualizada");owl.owlCarousel({items:5,autoPlay:4000,stopOnHover:true,pagination:false,itemsDesktop:[1199,5],itemsDesktopSmall:[980,5],itemsTablet:[768,4],itemsTabletSmall:false,itemsMobile:[479,3],});$(".next_actualizada").click(function(){owl.trigger('owl.prev');})
+$(".prev_actualizada").click(function(){owl.trigger('owl.next');})});</script>
+
+
+
+
+<!-- Peliculas  -->
+
+
+ <header><h2>Películas</h2> <span>100<a href=https://repelishd.tv/pelicula/ class=see-all>Ver todo</a></span></header>
+ <div id=movload class=load_modules>Cargando...</div>
+ <div class=items><article id=post-featured-10248 class="item movies"><div class="  visible-desktop"></div><div class=poster><img class=lazy src="https://i.ibb.co/7YqDS2d/San-Andr-s-397400007-large.jpg" data-src=https://i.ibb.co/7YqDS2d/San-Andr-s-397400007-large.jpg alt="La falla de San Andrés"> <div class=featu>Top 1</div><a href="#"><div class=see></div></a></div><div class="data dfeatur"><h3><a href="#">La falla de San Andrés</a></h3><span>2015</span></div></article><article id=post-featured-10248 class="item movies"><div class="  visible-desktop"></div><div class=poster><img class=lazy src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQmFnQTerYF1hkcpUfuuBEPXdhcpYb3hBA1fszoGXW3rB9xUl4q" data-src=https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQmFnQTerYF1hkcpUfuuBEPXdhcpYb3hBA1fszoGXW3rB9xUl4q alt="Encantada"><div class=featu>Top 2</div><a href="#"><div class=see></div></a></div><div class="data dfeatur"><h3><a href="#">Encantada</a></h3><span>2007</span></div></article><article id=post-featured-10248 class="item movies"><div class="  visible-desktop"></div><div class=poster><img class=lazy src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTKoR3UhdeCz2K6KzZGZrj_w_70NSfDcTOHUvbb-CbJfbEpU-zS" data-src=https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTKoR3UhdeCz2K6KzZGZrj_w_70NSfDcTOHUvbb-CbJfbEpU-zS alt="Jack, el cazagi gantes"><div class=featu>Top 3</div><a href="#"><div class=see></div></a></div><div class="data dfeatur"><h3><a href="#">Jack, el cazagi gantes</a></h3><span>2013</span></div></article>
+ <article id=post-featured-10248 class="item movies"><div class="  visible-desktop"></div><div class=poster><img class=lazy src="https://i.ibb.co/PC3qxR1/A-Qu-le-pas-a-lunes-442631083-large.jpg" data-src=https://i.ibb.co/PC3qxR1/A-Qu-le-pas-a-lunes-442631083-large.jpg alt="¿Qué le pasó a Lunes?" ><div class=featu>Top 4</div><a href="#"><div class=see></div></a></div><div class="data dfeatur"><h3><a href="#">¿Qué le pasó a Lunes?</a></h3><span>2017</span></div></article><article id=post-featured-10248 class="item movies"><div class="  visible-desktop"></div><div class=poster><img class=lazy src="https://i.ibb.co/XDvjxYB/world-war-z-150505659-large.jpg" data-src=https://i.ibb.co/XDvjxYB/world-war-z-150505659-large.jpg alt="Guerra mundial z" ><div class=featu>Top 5</div><a href="#"><div class=see></div></a></div><div class="data dfeatur"><h3><a href="#">Guerra mundial z</a></h3><span>2013</span></div></article><article id=post-featured-10248 class="item movies"><div class="  visible-desktop"></div><div class=poster><img class=lazy src="https://i.pinimg.com/564x/cb/19/68/cb196893ea55f237ac8f7aca02aa7723.jpg" data-src=https://i.pinimg.com/564x/cb/19/68/cb196893ea55f237ac8f7aca02aa7723.jpg alt="Estación Zombie"><div class=featu>Top 6</div><a href="#"><div class=see></div></a></div><div class="data dfeatur"><h3><a href="#">Estación Zombie</a></h3><span>2016</span></div></article>
+ <article id=post-featured-10248 class="item movies"><div class="  visible-desktop"></div><div class=poster><img class=lazy src="https://r1.abcimg.es/resizer/resizer.php?imagen=https%3A%2F%2Fstatic1.abc.es%2Fmedia%2Fpeliculas%2F000%2F043%2F212%2Fun-don-excepcional-1.jpg&nuevoancho=690&medio=abc" data-src=https://r1.abcimg.es/resizer/resizer.php?imagen=https%3A%2F%2Fstatic1.abc.es%2Fmedia%2Fpeliculas%2F000%2F043%2F212%2Fun-don-excepcional-1.jpg&nuevoancho=690&medio=abc alt="Un don excepcional"><div class=featu>Top 7</div><a href="#"><div class=see></div></a></div><div class="data dfeatur"><h3><a href="#">Un don excepcional</a></h3><span>2017</span></div></article><article id=post-featured-10248 class="item movies"><div class="  visible-desktop"></div><div class=poster><img class=lazy src="https://pbs.twimg.com/media/ESEuAhyXYAArzV7.jpg" data-src=https://pbs.twimg.com/media/ESEuAhyXYAArzV7.jpg alt="Violet y Finch"><div class=featu>Top 8</div><a href="#"><div class=see></div></a></div><div class="data dfeatur"><h3><a href="#">Violet y Finch</a></h3><span>2020</span></div></article><article id=post-featured-10248 class="item movies"><div class="  visible-desktop"></div><div class=poster><img class=lazy src="https://es.web.img3.acsta.net/pictures/18/07/27/13/53/2854483.jpg" data-src=https://es.web.img3.acsta.net/pictures/18/07/27/13/53/2854483.jpg alt="Todos los chicos de los que me enamore"><div class=featu>Top 9</div><a href="#"><div class=see></div></a></div><div class="data dfeatur"><h3><a href="#">Todos los chicos de los que me enamore</a></h3><span>2018</span></div></article><article id=post-featured-10248 class="item movies"><div class="  visible-desktop"></div><div class=poster><img class=lazy src="https://i.ibb.co/3RN5bcd/Insidious-733811348-large.jpg" data-src=https://i.ibb.co/3RN5bcd/Insidious-733811348-large.jpg alt="La noche del demonio"><div class=featu>Top 18</div><a href="#"><div class=see></div></a></div><div class="data dfeatur"><h3><a href="#">La noche del demonio</a></h3><span>2010</span></div></article>
+
+
+
+<!-- Peliculas  -->
+
+
+
+
+
+</div><header><h2>Últimas Series Agregadas</h2> <span>68 <a href=https://repelishd.tv/serie/ class=see-all>Ver todo</a></span></header><div id=slider-tvshows class="animation-1 slider"><article class=item id=post-7063><div class=image> <a href=https://repelishd.tv/serie/13-reasons-why-online-espanol-3178u/><img class=lazy src="data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%201%201'%3E%3C/svg%3E" data-src=//image.tmdb.org/t/p/w500/sZb21d6EWKAEKZ9GrLQeMwX4cWN.jpg alt="Por 13 Razones / 13 Reasons Why"></a> <a href=https://repelishd.tv/serie/13-reasons-why-online-espanol-3178u/><div class=data><h3 class="title">Por 13 Razones / 13 Reasons Why</h3> <span>2017</span></div> </a> <span class=item_type><i class=icon-live_tv></i> SERIE</span></div></article><article class=item id=post-11597><div class=image> <a href=https://repelishd.tv/serie/stargirl/><img class=lazy src="data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%201%201'%3E%3C/svg%3E" data-src=//image.tmdb.org/t/p/w500/b5lKppEquuQWm6fTjGcu2g8FJgs.jpg alt=Stargirl></a> <a href=https://repelishd.tv/serie/stargirl/><div class=data><h3 class="title">Stargirl</h3> <span>2020</span></div> </a> <span class=item_type><i class=icon-live_tv></i> SERIE</span></div>
+ 						</article><article class=item id=post-11828><div class=image> <a href=https://repelishd.tv/serie/jeffrey-epstein-asquerosamente-rico/><img class=lazy src="data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%201%201'%3E%3C/svg%3E" data-src=//image.tmdb.org/t/p/w500/cqUS5sJJB3DCbxgZpccMstyKPy6.jpg alt="Jeffrey Epstein: Asquerosamente rico"></a> <a href=https://repelishd.tv/serie/jeffrey-epstein-asquerosamente-rico/><div class=data><h3 class="title">Jeffrey Epstein: Asquerosamente rico</h3> <span>2020</span></div> </a> <span class=item_type><i class=icon-live_tv></i> SERIE</span></div></article><article class=item id=post-2639><div class=image> <a href=https://repelishd.tv/serie/rick-y-morty-online-espanol-767yh7/><img class=lazy src="data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%201%201'%3E%3C/svg%3E" data-src=//image.tmdb.org/t/p/w500/eV3XnUul4UfIivz3kxgeIozeo50.jpg alt="Rick y Morty"></a> <a href=https://repelishd.tv/serie/rick-y-morty-online-espanol-767yh7/><div class=data><h3 class="title">Rick y Morty</h3> <span>2013</span></div> </a> <span class=item_type><i class=icon-live_tv></i> SERIE</span></div>
+ 						</article><article class=item id=post-11247><div class=image> <a href=https://repelishd.tv/serie/the-last-kingdom-online/><img class=lazy src="data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%201%201'%3E%3C/svg%3E" data-src=//image.tmdb.org/t/p/w500/wUFIEJCSmurmXPX0mP0cEgoltrk.jpg alt="The Last Kingdom"></a> <a href=https://repelishd.tv/serie/the-last-kingdom-online/><div class=data><h3 class="title">The Last Kingdom</h3> <span>2015</span></div> </a> <span class=item_type><i class=icon-live_tv></i> SERIE</span></div></article><article class=item id=post-172><div class=image> <a href=https://repelishd.tv/serie/la-casa-de-papel-online-70501/><img class=lazy src="data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%201%201'%3E%3C/svg%3E" data-src=//image.tmdb.org/t/p/w500/dQtiDcc8N1joORj3E6oWC8vZVCb.jpg alt="La Casa de Papel"></a> <a href=https://repelishd.tv/serie/la-casa-de-papel-online-70501/><div class=data><h3 class="title">La Casa de Papel</h3> <span>2017</span></div> </a> <span class=item_type><i class=icon-live_tv></i> SERIE</span></div>
+ 						</article><article class=item id=post-10670><div class=image> <a href=https://repelishd.tv/serie/carta-al-rey/><img class=lazy src="data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%201%201'%3E%3C/svg%3E" data-src=//image.tmdb.org/t/p/w500/eOobdXCqm8288Wz2nJXUetX1sAD.jpg alt="Carta al rey"></a> <a href=https://repelishd.tv/serie/carta-al-rey/><div class=data><h3 class="title">Carta al rey</h3> <span>2020</span></div> </a> <span class=item_type><i class=icon-live_tv></i> SERIE</span></div></article><article class=item id=post-10654><div class=image> <a href=https://repelishd.tv/serie/better-call-saul/><img class=lazy src="data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%201%201'%3E%3C/svg%3E" data-src=//image.tmdb.org/t/p/w500/cU0kAjGjA6d2XjBzJMUIEVKiGDb.jpg alt="Better Call Saul"></a> <a href=https://repelishd.tv/serie/better-call-saul/><div class=data><h3 class="title">Better Call Saul</h3> <span>2015</span></div> </a> <span class=item_type><i class=icon-live_tv></i> SERIE</span></div></article><article class=item id=post-10587><div class=image> <a href=https://repelishd.tv/serie/toy-boy/><img class=lazy src="data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%201%201'%3E%3C/svg%3E" data-src=//image.tmdb.org/t/p/w500/6PICbFc9JyiiUwwZws7eB4EYNMm.jpg alt="Toy Boy"></a> <a href=https://repelishd.tv/serie/toy-boy/><div class=data><h3 class="title">Toy Boy</h3> <span>2019</span></div> </a> <span class=item_type><i class=icon-live_tv></i> SERIE</span></div>
+ 						</article></div><div id=dt-seasons class="animation-2 items"><article class="item se seasons" id=post-10354><div class=poster> <img class=lazy src="data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%201%201'%3E%3C/svg%3E" data-src=//image.tmdb.org/t/p/w185/34KjZaKgBnq6BWtlzZCqqWVKfzj.jpg alt="Star Wars: The Clone Wars: Temporada 7"><div class="season_m animation-1"> <a href=https://repelishd.tv/temporada/star-wars-the-clone-wars-temporada-7/> <span class=a>temporada</span> <span class=b>7</span> <span class=c>Star Wars: The Clone Wars</span> </a></div></div><div class=data><h3><a href=https://repelishd.tv/temporada/star-wars-the-clone-wars-temporada-7/>Temporada 7</a></h3> <span>Feb. 21, 2020</span></div></article></div><div class="sidebar scrolling"><div class=fixed-sidebar-blank><aside id=custom_html-60 class="widget_text widget widget_custom_html"><h2 class="widget-title">Lo mejor de la semana:</h2><div class="textwidget custom-html-widget"><div style="position: relative;"><center> <a href="" rel="nofollow noreferrer noopener"> <img class="lazy loaded" src=https://es.web.img3.acsta.net/pictures/17/04/27/15/42/265886.jpg data-src=https://es.web.img3.acsta.net/pictures/17/04/27/15/42/265886.jpg title="Merlí" data-was-processed=true style=" max-width: 100%; "> </a></center></div></div>
+ 							</aside><aside id=dtw_content_views-63 class="widget doothemes_widget"><h2 class="widget-title">Contenido Destacado</h2><div class="dtw_content dt_views_count"><article class=w_item_a id=post-2320> <a href="peliculas/venom.php"><div class=image> <img class=lazy src="data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%201%201'%3E%3C/svg%3E" data-src=//image.tmdb.org/t/p/w300/VuukZLgaCrho2Ar8Scl9HtV3yD.jpg alt=Venom><div class=data><h3>Venom</h3> <span class=wdate>2018</span></div><div class=mepo> <span class=quality>HD</span></div></div> </a></article><article class=w_item_a id=post-500> <a href=https://repelishd.tv/pelicula/proyecto-rampage-online-2018/><div class=image> <img class=lazy src="data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%201%201'%3E%3C/svg%3E" data-src=//image.tmdb.org/t/p/w300/wrqUiMXttHE4UBFMhLHlN601MZh.jpg alt="Proyecto Rampage"><div class=data><h3>Proyecto Rampage</h3> <span class=wdate>2018</span></div><div class=mepo> <span class=quality>HD</span></div></div> </a></article><article class=w_item_a id=post-3252> <a href=https://repelishd.tv/pelicula/ver-dragon-ball-super-broly-online-espanol-90326/><div class=image> <img class=lazy src="data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%201%201'%3E%3C/svg%3E" data-src=//image.tmdb.org/t/p/w300/6OTRuxpwUUGbmCX3MKP25dOmo59.jpg alt="Dragon Ball Super: Broly"><div class=data><h3>Dragon Ball Super: Broly</h3> <span class=wdate>2018</span></div><div class=mepo> <span class=quality>HD</span></div></div> </a></article></div></aside></div></div></div></div>
+
+ 							<?php
+							include ("wp-includes/p-p/footer.php");
+							?>
+
+
+ 						</div> <script src="wp-includes/js/ss-1.js"></script> <script src="wp-includes/js/script.js"></script> <script src="wp-includes/js/ss-2.js"></script> <script src="wp-includes/js/script1.js"></script> <script src="wp-includes/js/script2.js"></script> <div id=oscuridad></div> <script src="wp-includes/js/script3.js"></script><script async src="wp-includes/js/script3.js"></script></body></html>
